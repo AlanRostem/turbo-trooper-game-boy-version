623b37232ac5edf1ecbef862c6d0b6ab1dc81c0a
Alan Rostem <arostem@gmail.com>
2022-01-23 16:56:38 +0100

    sprite test

--- src/main.c
+++ src/main.c
@@ -1,5 +1,14 @@
-#include "include/Test.h"
+#include <gb/gb.h>
+#include <gb/drawing.h>
+
+#include "assets/sprites/CrackedStoneTile.h"
+
+const uint8_t SCREEN_WIDTH = 160u;
+const uint8_t SCREEN_HEIGHT = 144u;
 
 void main(){
-    Test();
+    set_sprite_data(0, 0, CrackedStoneTile);
+    set_sprite_tile(0, 0);
+    move_sprite(0, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+    SHOW_SPRITES;
 }
