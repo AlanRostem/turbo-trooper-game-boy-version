eca8a33148dc1ce50f80c15e7efdbd5bbdb5f8da
Alan Rostem <arostem@gmail.com>
2022-01-24 11:28:37 +0100

    yeah i just need to accept that im wrong

--- src/graphics/Sprite.c
+++ src/graphics/Sprite.c
@@ -1,4 +1,9 @@
 #include "Sprite.h"
+/*
+#ifndef NDEBUG
+#include <stdio.h>
+#endif
+*/
 
 static uint8_t sprites_used = 0;
 
@@ -12,18 +17,25 @@ void Sprite_create(Sprite* sprite, uint8_t* image_data, uint8_t tile_count)
     sprite->data = image_data;
     sprite->sprite_number = sprites_used++;
     sprite->tile_count = tile_count;
-#if 0
+    /*
+#ifndef NDEBUG
     assert(sprite->sprite_number == MAX_HARDWARE_SPRITES);
 #endif
+    */
 }
 
 
-extern void Sprite_load(Sprite* sprite)
+void Sprite_load(Sprite* sprite)
 {
     set_sprite_data(0, sprite->tile_count, sprite->data);
 }
 
-extern void Sprite_set_tile(Sprite* sprite, uint8_t tile)
+void Sprite_set_tile(Sprite* sprite, uint8_t tile)
 {
     set_sprite_tile(sprite->sprite_number, tile);
+}
+
+void Sprite_set_position(Sprite* sprite, uint8_t x, uint8_t y) 
+{
+    move_sprite(sprite->sprite_number, x, y);
 }
--- src/graphics/Sprite.h
+++ src/graphics/Sprite.h
@@ -9,6 +9,7 @@ typedef struct {
 
 extern void Graphics_show_sprites();
 
-extern void Sprite_create(Sprite* sprite, const uint8_t* image_data, uint8_t tile_count);
+extern void Sprite_create(Sprite* sprite, uint8_t* image_data, uint8_t tile_count);
 extern void Sprite_load(Sprite* sprite);
 extern void Sprite_set_tile(Sprite* sprite, uint8_t tile);
+extern void Sprite_set_position(Sprite* sprite, uint8_t x, uint8_t y);
