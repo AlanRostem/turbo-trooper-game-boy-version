c9100879eedb8c4fd80652ff5e5f11a060c64148
Alan Rostem <arostem@gmail.com>
2022-01-26 09:45:35 +0100

    putting sprites in proper format

--- src/graphics/Sprite.c
+++ src/graphics/Sprite.c
@@ -4,15 +4,13 @@
 #include <gb/drawing.h>
 #include <stdio.h>
 
-void Sprite_create(Sprite* sprite, uint8_t tile_count, uint8_t* image_data)
-{
+void Sprite_create(Sprite* sprite, uint8_t tile_count, uint8_t* image_data) {
     sprite->image_data = image_data;
     sprite->tile_count = tile_count;
     sprite->tile_position_in_memory = Memory_allocate_and_write_sprite_data(sprite->tile_count, sprite->image_data);
 }
 
-void Sprite_allocate_on_display(Sprite* sprite)
-{
+void Sprite_allocate_on_display(Sprite* sprite) {
     sprite->hardware_sprite_number = Memory_generate_hardware_sprite_number();
     set_sprite_tile(sprite->hardware_sprite_number, sprite->tile_position_in_memory);
 }
@@ -21,8 +19,7 @@ void Sprite_free_from_display(Sprite *sprite) {
     Memory_free_hardware_sprite_number(sprite->hardware_sprite_number);
 }
 
-void Sprite_set_frame(Sprite* sprite, uint8_t frame)
-{
+void Sprite_set_frame(Sprite* sprite, uint8_t frame) {
     set_sprite_tile(sprite->hardware_sprite_number, sprite->tile_position_in_memory + frame);
 }
 
