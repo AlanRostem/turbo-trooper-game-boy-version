f367e582b61a43817a39dfd1342bf19f8fddccdb
Alan Rostem <arostem@gmail.com>
2022-01-27 12:21:42 +0100

    it worked all along! just needed correctframe

--- src/main.c
+++ src/main.c
@@ -17,8 +17,8 @@ MetaSprite player_meta_sprite;
 uint8_t player_meta_sprite_numbers[4];
 
 uint8_t player_run_animation_frames[4][4] = {
-    {8, 8, 10, 10},
-    {9, 9, 11, 11},
+    {8, 8, 10, 8},
+    {9, 9, 11, 9},
     {12, 14, 16, 18},
     { 13, 15, 17, 19},
 };
@@ -36,10 +36,10 @@ int main()
     SingleSprite_occupy_display(&bricks_single_sprite);
     SingleSprite_set_position(&bricks_single_sprite, 40, 80);
 
-    SpriteAnimation_create(&player_run_animations[0], 4, player_run_animation_frames[0]);
-    SpriteAnimation_create(&player_run_animations[1], 4, player_run_animation_frames[1]);
-    SpriteAnimation_create(&player_run_animations[2], 4, player_run_animation_frames[2]);
-    SpriteAnimation_create(&player_run_animations[3], 4, player_run_animation_frames[3]);
+    static uint8_t i;
+    for (i = 0; i < 4; i++) {
+        SpriteAnimation_create(&player_run_animations[i], 4, player_run_animation_frames[i]);
+    }
 
     MetaSprite_create(&player_meta_sprite, META_SPRITE_TILE_DIMENSION_16x16, 22, PlayerTestSprite);
     MetaSprite_occupy_display(&player_meta_sprite, player_meta_sprite_numbers);
@@ -48,12 +48,11 @@ int main()
 
     Graphics_show_sprites();
     while(1) {
-        if (has_frame_timer_exceeded_wait_time(&player_test_animation_elapsed_frame_time, 120, FRAME_DELTA)) {
-            static uint8_t i;
+        if (has_frame_timer_exceeded_wait_time(&player_test_animation_elapsed_frame_time, 150, FRAME_DELTA)) {
             for (i = 0; i < 4; i++) {
-                SpriteAnimation_loop(&player_run_animations[i]);
                 MetaSprite_set_frame(&player_meta_sprite, i,
                                      SpriteAnimation_get_current_frame(&player_run_animations[i]));
+                SpriteAnimation_loop(&player_run_animations[i]);
             }
         }
 
