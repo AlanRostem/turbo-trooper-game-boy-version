da2ef42ecad0127c33a223096f13383c8e52982b
Alan Rostem <arostem@gmail.com>
2022-02-06 12:37:31 +0100

    tested tiles it works

--- src/main.c
+++ src/main.c
@@ -5,13 +5,14 @@
 int main() {
 
     Graphics_create();
-    set_bkg_submap(0, 0, 20, 16, TestTileMap, 255);
+    set_bkg_submap(0, 0, 32, 16, TestTileMap, 255);
 
     Graphics_show();
 
     GameWorld_create();
     while(1) {
         wait_vbl_done();
+        scroll_bkg(1, 0);
         GameWorld_process();
     }
     return 0;
