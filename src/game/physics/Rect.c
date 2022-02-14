a10c9744f3505b7d577c0292b359ff87917094b0
Alan Rostem <arostem@gmail.com>
2022-02-14 13:52:29 +0100

    inline

--- src/game/physics/Rect.c
+++ src/game/physics/Rect.c
@@ -1,15 +1,3 @@
 #include "Rect.h"
 
-bool_t Rect_overlap(Rect* a, Rect* b) {
-    return  (a->pos.y + a->size.y) > b->pos.y
-        &&  a->pos.y < (b->pos.y + b->size.y)
-        &&  (a->pos.x + a->size.x) > b->pos.x
-        &&  a->pos.x < (b->pos.x + b->size.x);
-}
 
-bool_t Rect_overlap_tile(Rect *rect, int16_t tile_x, int16_t tile_y) {
-    return  (rect->pos.y + rect->size.y) > tile_y * TILE_SIZE
-            && rect->pos.y < ((tile_y + 1) * TILE_SIZE)
-            && (rect->pos.x + rect->size.x) > tile_x * TILE_SIZE
-            && rect->pos.x < ((tile_x + 1) * TILE_SIZE);
-}
--- src/game/physics/Rect.h
+++ src/game/physics/Rect.h
@@ -14,5 +14,16 @@ typedef struct {
     Vector2i16 pos;
 } Rect;
 
-extern bool_t Rect_overlap(Rect* a, Rect* b);
-extern bool_t Rect_overlap_tile(Rect* a, int16_t tile_x, int16_t tile_y);
+inline bool_t Rect_overlap(Rect* a, Rect* b) {
+    return  (a->pos.y + a->size.y) > b->pos.y
+            &&  a->pos.y < (b->pos.y + b->size.y)
+            &&  (a->pos.x + a->size.x) > b->pos.x
+            &&  a->pos.x < (b->pos.x + b->size.x);
+}
+
+inline bool_t Rect_overlap_tile(Rect *rect, int16_t tile_x, int16_t tile_y) {
+    return  (rect->pos.y + rect->size.y) > tile_y * TILE_SIZE
+            && rect->pos.y < ((tile_y + 1) * TILE_SIZE)
+            && (rect->pos.x + rect->size.x) > tile_x * TILE_SIZE
+            && rect->pos.x < ((tile_x + 1) * TILE_SIZE);
+}
