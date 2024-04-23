b32858162432435fb08a1182b7fe824ef809ca40
AlanRostem <arostem@gmail.com>
2024-04-23 14:06:40 +0200

    gonna do everything from main.c for now

--- src/main.c
+++ src/main.c
@@ -1,18 +1,45 @@
-#include "graphics/Graphics.h"
-#include "game/GameWorld.h"
+#include "assets/sprites/TileSet.h"
 #include "assets/tile_maps/TestTileMap.h"
+
+#define TILE_MAP_WIDTH 48 // This is a test value for now
+#define TILE_MAP_HEIGHT 16
+#define MAX_RENDERED_TILE_MAP_WIDTH 32
+#define MAX_ON_SCREEN_TILE_MAP_WIDTH 20
+#define OUT_OF_SCREEN_RENDERED_TILE_WIDTH_HALF 6
+#define OUT_OF_SCREEN_RENDERED_TILE_WIDTH 12
+#define TILE_SIZE 8
+
 
 int main() {
 
-    Graphics_create();
-    Graphics_show_start_of_tile_map(TestTileMap);
+    // WRAM variables
+    static uint8_t joypad_input;
+    static uint8_t tile_offset_x = 0;
 
-    GameWorld_create();
+    // Write tile set to VRAM
+    set_bkg_2bpp_data(
+        0, 
+        38, 
+        TileSet);
 
-    Graphics_show();
+    // Set background tiles
+    uint8_t tile_offset_x = OUT_OF_SCREEN_RENDERED_TILE_WIDTH_HALF;
+    set_bkg_submap(tile_offset_x, 0, 
+        MAX_RENDERED_TILE_MAP_WIDTH, 
+        TILE_MAP_HEIGHT, 
+        TestTileMap, 
+        TILE_MAP_WIDTH);
+    move_bkg(tile_offset_x * TILE_SIZE, 0);
+
+    // Enable bg and sprites
+    SHOW_SPRITES;
+    SHOW_BKG;
+
     while(1) {
         wait_vbl_done();
-        GameWorld_process();
+
+        joypad_input = joypad();
+
     }
 
     return 0;
